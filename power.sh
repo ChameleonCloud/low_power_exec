b95ba2db0b0d10c7ca8371ba6f7169ae19f143ac
Joe Stubbs <jstubbs@tacc.utexas.edu>
2018-04-03 18:31:43 -0500

    make sure no collectdPower container exists at start up

--- power.sh
+++ power.sh
@@ -18,4 +18,10 @@
 HOSTNAME="${COLLECTD_HOSTNAME:-localhost}"
 INTERVAL="${COLLECTD_INTERVAL:-60}"
 
+# make sure there is no container with name collectdPower; the run below sets the --rm flag but it is 
+# possible the container will be left if, for instance, docker is restarted while the container is 
+# running
+docker rm -f collectdPower
+
+# run the actual container
 docker run --rm --name collectdPower -e HOSTNAME=$HOSTNAME -e INTERVAL=$INTERVAL -e SSH_PASSWORD=<the_password> jstubbs/low_power_exec
