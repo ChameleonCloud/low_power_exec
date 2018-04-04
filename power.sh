338d69b8b80f00f6d75457973856896f8ebcd670
Joe Stubbs <jstubbs@tacc.utexas.edu>
2018-04-04 09:50:15 -0500

    suppress errors

--- power.sh
+++ power.sh
@@ -20,8 +20,8 @@ INTERVAL="${COLLECTD_INTERVAL:-60}"
 
 # make sure there is no container with name collectdPower; the run below sets the --rm flag but it is 
 # possible the container will be left if, for instance, docker is restarted while the container is 
-# running
-docker rm -f collectdPower
+# running; suppress errors as failures are expected.
+docker rm -f collectdPower &>/dev/null
 
 # run the actual container
 docker run --rm --name collectdPower -e HOSTNAME=$HOSTNAME -e INTERVAL=$INTERVAL -e SSH_PASSWORD=<the_password> jstubbs/low_power_exec
