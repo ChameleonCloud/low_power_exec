2f694c526f06eadeff7a4b5e5e29de6e71d21c24
Jason <jasonanderson@uchicago.edu>
2019-01-31 19:57:46 -0600

    Beautify

--- collect_corsa.py
+++ collect_corsa.py
@@ -17,6 +17,7 @@ ep_netns = '/netns'
 
 metric_template = 'PUTVAL "{host}/corsa-{section}/{port}-{metric}" {timestamp}:{value}'
 
+
 class CorsaClient():
     def __init__(self, address, token, verify=None):
         self.address = address
@@ -52,157 +53,157 @@ class CorsaClient():
     def get_equipment(self):
         return self.get_path(ep_equipment)
 
-
     # GET EQUIPMENT SLOTS
     #   200 OK
+
     def get_equipment_slots(self):
         return self.get_path(ep_equipment + '/slots')
 
-
     # GET EQUIPMENT SLOT
     #   200 OK
+
     def get_equipment_slot(self, slot):
         return self.get_path(ep_equipment + '/slots/' + str(slot))
-
 
     # GET EQUIPMENT SLOT MODULE
     #   200 OK
     #   404 Not Found
+
     def get_equipment_slot_module(self, slot):
         return self.get_path(ep_equipment + '/slots/' + str(slot) + '/module')
 
-
     # GET EQUIPMENT FANTRAYS
     #   200 OK
+
     def get_equipment_fantrays(self):
         return self.get_path(ep_equipment + '/fantrays')
-
 
     # GET EQUIPMENT FANTRAY
     #   200 OK
     #   404 Not Found
+
     def get_equipment_fantray(self, fantray):
         return self.get_path(ep_equipment + '/fantrays/' + str(fantray))
-
 
     # GET EQUIPMENT FANS
     #   200 OK
     #   404 Not Found
+
     def get_equipment_fans(self, fantray):
         return self.get_path(ep_equipment + '/fantrays/' + str(fantray) + '/fans')
-
 
     # GET EQUIPMENT FAN
     #   200 OK
     #   404 Not Found
+
     def get_equipment_fan(self, fantray, fan):
         path = ep_equipment + '/fantrays/' + \
             str(fantray) + '/fans/' + str(fan)
 
         return self.get_path(path)
 
-
     # GET EQUIPMENT LED
     #   200 OK
+
     def get_equipment_led(self):
         return self.get_path(ep_equipment + '/led')
 
-
     # GET EQUIPMENT BOARDTEMP
     #   200 OK
+
     def get_equipment_boardtemp(self):
         return self.get_path(ep_equipment + '/boardtemp')
 
-
     # GET EQUIPMENT CPU
     #   200 OK
+
     def get_equipment_cpu(self):
         return self.get_path(ep_equipment + '/cpu')
 
-
     # GET EQUIPMENT PSUS
     #   200 OK
+
     def get_equipment_psus(self):
         return self.get_path(ep_equipment + '/psus')
 
-
     # GET EQUIPMENT PSU
     #   200 OK
+
     def get_equipment_psu(self, psu):
         return self.get_path(ep_equipment + '/psus/' + str(psu))
 
-
     # GET EQUIPMENT EEPROMS
     #   200 OK
+
     def get_equipment_eeproms(self):
         return self.get_path(ep_equipment + '/eeproms')
 
-
     # GET EQUIPMENT EEPROM
     #   200 OK
+
     def get_equipment_eeprom(self, eeprom):
         return self.get_path(ep_equipment + '/eeproms/' + str(eeprom))
 
-
     # GET EQUIPMENT AIRFLOW
     #   200 OK
+
     def get_equipment_airflow(self):
         return self.get_path(ep_equipment + '/airflow')
 
-
     # GET NETNS
     #   200 OK
+
     def get_netns_info(self):
         return self.get_path(ep_netns)
 
-
     # GET NETNS SPECIFIC
     #   200 OK
+
     def get_netns(self, netns):
         return self.get_path(ep_netns + '/' + str(netns))
 
-
     # GET NETNS TUNNELS
     #   200 OK
+
     def get_netns_tunnels(self, netns):
         return self.get_path(ep_netns + '/' + str(netns) + '/tunnels')
 
-
     # GET NETNS TUNNEL
     #   200 OK
+
     def get_netns_tunnel(self, netns, tunnel):
         return self.get_path(ep_netns + '/' + str(netns) + '/tunnels/' + str(tunnel))
-
 
     # GET PORTS
     #   200 OK
     #   403 Forbidden
+
     def get_ports(self):
         return self.get_path(ep_ports)
-
 
     # GET PORT
     #   200 OK
     #   403 Forbidden
+
     def get_port(self, port):
         return self.get_path(ep_ports + '/' + str(port))
-
 
     # GET PORT TRAFFIC CLASSES
     #   200 OK
     #   403 Forbidden
+
     def get_port_traffic_class(self, port):
         return self.get_path(ep_ports + '/' + str(port) + '/traffic-classes')
 
-
     # GET QUEUE PROFILES
     #   200 OK
+
     def get_queue_profiles(self):
         return self.get_path(ep_qp)
 
-
     # GET STATS
     #   200 OK
+
     def get_stats(self):
         return self.get_path(ep_stats)
 
@@ -224,6 +225,7 @@ class CorsaClient():
                 path = path + '&ofport=' + str(ofport)
         return self.get_path(ep_stats + '/tunnels')
 
+
 def main():
     config = ConfigParser.ConfigParser()
     config.read('/etc/metrics/metrics.conf')
@@ -232,7 +234,8 @@ def main():
     sections = [s for s in config.sections() if s.startswith(section_header)]
 
     if not sections:
-        raise ValueError('Could not find any valid sections describing switches')
+        raise ValueError(
+            'Could not find any valid sections describing switches')
 
     for section in sections:
         identifier = section.replace(section_header, '')
@@ -244,7 +247,7 @@ def main():
         try:
             port_stats = client.get_stats_ports()
             for stat in port_stats['stats']:
-                for key, val in stat if key not in ['ifdesc', 'port']:
+                for key, val in stat.items():
                     print(metric_template.format(
                         host='test',
                         section=section,
@@ -253,6 +256,7 @@ def main():
                         timestamp=int(time.time()),
                         value=val
                     ))
+
 
 if __name__ == '__main__':
     main()
