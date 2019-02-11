cc287f4361c30e904851e47dd76d8aeca51ebb08
Jason <jasonanderson@uchicago.edu>
2019-02-11 15:48:56 -0600

    Change ownership of config file

--- collectd_wrapper.sh
+++ collectd_wrapper.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 command="${1:-power}"
+user=apim
+
+config_path=/etc/metrics/config.yml
+logs_path=/var/log/collectd.log
+# Create a copy of the metrics; we are going to be changing the ownership
+# so that the plugin code can read this as configuration.
+tmp_config_path=$(mktemp --tmpdir metrics-config.XXXXXX.yml)
+cat $config_path >$tmp_config_path && chown $user: $tmp_config_path
+touch $logs_path && chown $user: $logs_path
 
 python <<PYSCRIPT
 from jinja2 import Template
 from yaml import safe_load
 with open('/collectd.conf.j2') as f:
   template = Template(f.read())
-with open('/etc/metrics/config.yml') as f:
+with open('$tmp_config_path') as f:
   config = safe_load(f)
   # Merge command-specific configuration into parent scope
   config.update(config.get('$command'))
@@ -19,8 +28,5 @@ if [[ $? -gt 0 ]]; then
   echo "Failed to write collectd configuration."
   exit 1
 fi
-
-touch /var/log/collectd.log
-chown apim: /var/log/collectd.log
 
 exec collectd -f
